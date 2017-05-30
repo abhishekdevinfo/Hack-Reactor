83bb6f014677cf969496b17a8a0e4b2079010c7e
Abhishek Baliyan <abhi.info.dev@gmail.com>
2017-05-31 01:13:03 +0530

    Solution for Array to Object [Part 3]

---
+++ Module 0/Array-to-Object-[Part 3].js
@@ -0,0 +1,50 @@
+/**
+ * Write a function called "transformEmployeeData" that transforms some employee data from one format to another.
+
+The argument will look something like this:
+[
+    [
+        ['firstName', 'Joe'], ['lastName', 'Blow'], ['age', 42], ['role', 'clerk']
+    ],
+    [
+        ['firstName', 'Mary'], ['lastName', 'Jenkins'], ['age', 36], ['role', 'manager']
+    ]
+]
+
+Given that input, the return value should look like this:
+[
+    {firstName: 'Joe', lastName: 'Blow', age: 42, role: 'clerk'},
+    {firstName: 'Mary', lastName: 'Jenkins', age: 36, role: 'manager'}
+]
+
+Note that the input may have a different number of rows or different keys than the given sample. 
+
+For example, let's say the HR department adds a "tshirtSize" field to each employee record. Your code should flexibly accommodate that.
+
+Starter Code :
+function transformEmployeeData(array) {
+  // your code here
+}
+ */
+
+function transformEmployeeData(array) {
+  let arr = [];
+  let obj = {};
+  array.forEach(e => {
+    e.forEach(f => {
+      obj[f[0]] = f[1];
+    }); 
+    arr.push(obj);
+    obj = {};
+  });
+  return arr;
+}
+
+console.log(transformEmployeeData([
+    [
+        ['firstName', 'Joe'], ['lastName', 'Blow'], ['age', 42], ['role', 'clerk']
+    ],
+    [
+        ['firstName', 'Mary'], ['lastName', 'Jenkins'], ['age', 36], ['role', 'manager']
+    ]
+]));
