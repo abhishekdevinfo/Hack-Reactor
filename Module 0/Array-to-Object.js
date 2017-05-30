8650eb65f71f7638ed6480f0f92088b28b990ebe
Abhishek Baliyan <abhi.info.dev@gmail.com>
2017-05-31 00:26:39 +0530

    Solution for Array to Object

---
+++ Module 0/Array-to-Object.js
@@ -0,0 +1,42 @@
+/**
+ * Write a function 'transformFirstAndLast' that takes in an array, and returns an object with: 
+1) the first element of the array as the object's key, and 
+2) the last element of the array as that key's value.
+
+Example input:
+['Queen', 'Elizabeth', 'Of Hearts', 'Beyonce']
+
+Function's return value (output):
+{
+  Queen : 'Beyonce'
+}
+
+Do not change the input array. Assume all elements in the input array will be of type 'string'.
+
+Note that the input array may have a varying number of elements. Your code should flexibly accommodate that.
+
+E.g. it should handle input like:
+['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey']
+
+Function's return value (output):
+{
+  Kevin : 'Spacey'
+}
+
+Starter Code
+function transformFirstAndLast(array) {
+  // your code here
+}
+ */
+
+function transformFirstAndLast(array) {
+  let obj = {};
+  let last = array.length - 1;
+  for (let i = 0; i < array.length / 2; i += 1) {
+    obj[array[i]] = array[last];
+    last -= 1;
+  }
+  return obj
+}
+
+console.log(transformFirstAndLast(['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey']));
